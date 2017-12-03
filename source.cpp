a4e09efb64fbc5d5ab4fd2f02d0aba7541fc5ecc
Ryan Rupert <mudge25+code@gmail.com>
2017-12-02 16:05:54 -0800

    Third attempt worked

--- source.cpp
+++ source.cpp
@@ -1,24 +1,12 @@
+#include <string>
 #include <iostream>
-#include <cstdio>
-#include <sstream>
-
 #include <Windows.h>
 
-//source: https://stackoverflow.com/questions/45575863/how-to-print-utf-8-strings-to-stdcout-on-windows
-
-class MBuf: public std::stringbuf {
-public:
-    int sync() {
-        fputs( str().c_str(), stdout );
-        str( "" );
-        return 0;
-    }
-};
+//source: https://stackoverflow.com/a/45576563
 
 int main() {
-    SetConsoleOutputCP( CP_UTF8 );
-    setvbuf( stdout, nullptr, _IONBF, 0 );
-    MBuf buf;
-    std::cout.rdbuf( &buf );
-    std::cout << u8"Greek: αβγδ\n" << std::flush;
-} 
+    std::string test = u8"Greek: αβγδ; German: Übergrößenträger";
+    SetConsoleOutputCP(CP_UTF8);
+    std::cout << test;
+    std::cin.get();
+}
